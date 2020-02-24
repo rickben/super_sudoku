e6dbb26b9813533b014fa8eb145541c965d91e07
rickyBEN <ricky.benkovich@gmail.com>
2020-02-24 14:31:10 +0200

    24\2\20 - start

--- Game.h
+++ Game.h
@@ -13,12 +13,6 @@
  * solution: the board_game after getting the same values as
  * fixed_board and then determ_backtrack() for future hints
  * */
-struct list{
-    int** board_game;
-    int is_game_over;
-    int is_exit;
-} board,solution,board_fixed;
-
 
 /*
  *Game Summary:
@@ -35,91 +29,15 @@ struct list{
  *board_restart           - restarts the game
  * board_exit             - prints and exits the game
  * */
+struct curr_board{
+    int** board;
+}*curr_board;
+
 
 /*
  * Prints the current sudoku board
  * */
 void board_print();
 
-
-/*
- * Creates a new board game : initializes fixed_board.board_game
- * int the size of BOARD_LEN X BOARD_LEN
- *  Return 0 if a malloc error happened
- * */
-int fixed_create();
-
-/*
- * Sets the value to the board
- * as the user commanded
- * if it was a successful set then
- * return 1, else return 0
- * */
-int board_set();
-
-/*
- *Checks if the curr ent board is solvable
- * using valid_determ_backtrack()
- * */
-void board_validate();
-
-/*
- *Receive a possible value for the cell <x,y>
- * using the stored puzzle solution
- * @param x , y - row number , column number
- * */
-void board_hint(int x, int y);
-
-/*
- * Creates a new board game : initializes board.board_game
- * int the size of BOARD_LEN X BOARD_LEN
- *  * Return 0 if a malloc error happened
-
- * */
-int board_create();
-
-/*
- * Free all memory resources associated with a game
- * */
-void board_destroy();
-
-/*
- * Uses suitable functions to initialize and
- * restart the game
- * */
-int board_restart(int H);
-
-/*
- * If board exit returns 1, the game should
- * terminate after the print
- * */
-void board_exit();
-
-/*
- * Creates a new board game : initializes solution.board_game
- * and then sets the same values of the board.board_game
- * int the size of BOARD_LEN X BOARD_LEN
- * Return 0 if a malloc error happened
- * */
-int solution_create();
-
-/*
- * Restarts the game by calling the suitable
- * functions
- * */
-int game_restart(int H);
-
-
-/*
- * checks if there is no empty cells, if there is no
- * calls board_solved
- * */
-void check_if_board_solved();
-
-/*
- *restarts the game by calling the
- * suitable functions
- * */
-void start_over();
-
+void my_exit();
 #endif
--- MainAux.c
+++ MainAux.c
@@ -1,186 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "MainAux.h"
-const int BOARD_LEN=9;
-const int BLOCK_LEN=3;
+#include <math.h>
 
 
 
-
-
-void print_exit_game() {
-    printf("Exiting...\n");
-}
-
-
-void separator_row() {
-    printf("----------------------------------\n");
-}
-
-
-int get_hint_size() {
-    int H, value;
-    printf("Please enter the number of cells to fill [0-80]:\n");
-    while ((value=scanf("%d",&H)) != 1 || H < 0 || H > 80) {
-        if(feof(stdin)){
-            board_exit();
-            return -1;
+int* calc_block_size(int len){
+    int root = (int) (sqrt(len));
+    int* arr = calloc(2, sizeof(int*));
+    while (root > 0){
+        if(len % root == 0){
+            int n = root;
+            int m = (len / root);
+            arr[0] = n;
+            arr[1] = m;
+            return arr;
         }
-        if (value != 1) {
-            printf("Error: not a number\n");
-            board_exit();
-            return -1;
-        } else {
-            printf("Error: invalid number of cells to fill (should be between 0 and 80)\n");
-        }
-        fgetc(stdin);
-    }
-    return 81-H;
-}
-
-
-
-/*
- * Returns whether a cell in position <x,y>:
- * 0 - means not fixed (FALSE)
- * 1 - means fixed (TRUE)
- * */
-bool is_fixed(int x, int y) {
-    if(board_fixed.board_game[x][y]!=EMPTY)
-        return true;
-    else{
-        return false;
+        root--;
     }
 }
 
-
-bool find_empty_cell(int* row_pos, int* col_pos, int** matrix ) {
-    {
-        int i, j;
-        for(i=0;i<BOARD_LEN;i++)
-        {
-            for(j=0;j<BOARD_LEN;j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    *row_pos = i;
-                    *col_pos = j;
-                    return true;
-                }
-            }
+void separator_row(int height,int width) {
+    int i=0,j=0;
+//    printf("%d %d\n", height, width);
+    printf("-");
+    for(; i<height; i++){
+        printf("--");
+        for(; j<width-1; j++){
+        printf("---");
         }
-        return false;
+        printf("---");
+        j=0;
     }
+    printf("\n");
 }
 
-/*
- * Return false if there's a cell in the same row
- * with the same value, otherwise, returns true
- * */
-bool in_row(int row, int num, int** matrix) {
-    int j;
-    for (j = 0; j < BOARD_LEN; ++j) {
-        if (matrix[row][j] == num) {
-            if(num!=0) {
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-    }
-    return false;
-}
 
-/*
- * Return false if there's a cell in the same column
- * with the same value, otherwise, returns true
- * */
-bool in_col(int col, int num, int** matrix) {
-    int j;
-    for (j = 0; j < BOARD_LEN; ++j) {
-        if (matrix[j][col] == num) {
-            if (num != 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-    return false;
-}
-
-/*
- * Return false if there's a cell in the same block
- * with the same value, otherwise, returns true
- * */
-bool in_block(int x, int y, int num, int** matrix) {
+void board_print(int len) {
+    int* arr = calc_block_size(len);
+    int block_width = arr[0];
+    int block_height = arr[1];
     int i, j;
-    for (i = 0; i < BLOCK_LEN; ++i) {
-        for (j = 0; j < BLOCK_LEN; ++j) {
-            if (matrix[i+x][j+y] == num) {
-                if(num!=0) {
-                    return true;
-                }
+    for (i = 0; i < len; i++) {
+        if(i % block_width==0){
+            separator_row(block_height, block_width);
+        }
+        for (j = 0; j < len; j++) {
+            if(j%block_height==0){
+                if(j==0){
+                    printf("|");}
                 else{
-                    return false;
+                    printf(" |");
                 }
             }
+
+                printf("   ");
+
+
+
         }
+        printf(" |\n");
     }
-    return false;
+    separator_row(block_height, block_width);
 }
 
-
-/*
- * Returns whether a cell in position <x,y>:
- * means not valid (FALSE)
- * means valid (TRUE)
- * */
-bool is_valid_set(int x, int y, int num, int** matrix) {
-    int block_x = (x/3)*3;
-    int block_y = (y/3)*3;
-    return (!in_row(x,num,matrix) && !in_col(y,num,matrix) &&
-            !in_block(block_x,block_y,num,matrix) && matrix[x][y]==EMPTY);
-}
-
-
-
-int number_of_non_empty_vars(int *array){
-    int len = BOARD_LEN;
-    int cnt = 0;
-    int i = 0;
-    for(;i<len;i++){
-        if(array[i]!=0){
-            cnt++;
-        }
+void create_board(int len){
+    curr_board->board = calloc(len, sizeof(int*));
+    for (int i = 0; i < len; ++i) {
+        curr_board->board[i] = calloc(len, sizeof(int*));
     }
-    return cnt;
 }
-
-
-
-
-int prev_x(int x, int y) {
-    if(y==0)
-        if(x>0)
-            return x-1;
-        else
-            return 0;
-    else
-        return x;
-}
-
-
-int prev_y(int y){
-    if(y==0)
-        return BOARD_LEN-1;
-    else
-        return y-1;
-}
-
-void board_solved() {
-    board.is_game_over=1;
-    printf("Puzzle solved successfully\n");
-}
-
