4cd33f15e988d63193980b266f2f8f30ca257cb5
matan mula <matanmula@interai.co>
2020-02-25 13:11:05 +0200

    parser changes

--- Parser.h
+++ Parser.h
@@ -1,6 +1,8 @@
 #ifndef HW3_PARSER_H
 #define HW3_PARSER_H
 #include "MainAux.h"
-
-
+#include <string.h>
+int str_equals(char *str1, char *str2);
+int command_to_code(char* input) ;
+void execute_command();
 #endif
