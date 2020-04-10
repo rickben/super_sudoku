586ae3d12fe377bc3ac3df82bd7db260f25b4cdd
rickyBEN <ricky.benkovich@gmail.com>
2020-04-10 12:34:47 +0300

    10/4 - changed name of make-file to sudoku-console and added discription to Game.h

--- Game.h
+++ Game.h
@@ -3,34 +3,39 @@
 #include <stdio.h>
 #include "ListActions.h"
 
-
 /*
- * is_game_over is a flag that
- * represents if the board is solved,
- * board_game is the multi-dimensional array that
- * represents this board_game
- * board : the board_game which the player gets and changes
- * fixed_board : the board_game after the clean of leaving only
- * the number of cells, received by the user.
- * solution: the board_game after getting the same values as
- * fixed_board and then determ_backtrack() for future hints
+ * curr_board represents the current board game - whether its
+ * been loaded in Solve or Edit mode.
+ * curr_board->board : is the multi-dimensional array that represents
+ * this board game which the player can load, edit, change and save.
  * */
 
 /*
  *Game Summary:
  *
- *A BOARDLEN X BOARDLEN (rows X columns) puzzle board with only one player.
+ *A curr_board->len X curr_board->len (N X N) puzzle board with only one player.
  *
  *It supports the following functions:
  *
- *board_create            - creates a new game board
- *board_destroy           - free all memory of game
- *board_set               - sets a move on game board
- *board_valid             - checks if a move is valid
- *board_print             - prints the current board
- *board_restart           - restarts the game
- * board_exit             - prints and exits the game
+ * save                    - saves current board to specified file
+ * solve                   - sets Solve mode and loads the board from specified file
+ * edit                    - sets Edit mode and loads the board from specified file / empty 9x9 board
+ * board_set               - sets a move on game board
+ * validate                - checks if a move is valid
+ * hint                    - uses ILP to print hint for the value in the specified position
+ * generate                - randomly sets values for X empty cells, solves by ILP, and randomly leaves Y cells
+ * guess                   - uses LP to set legal values to board cells with probabilities higher than X
+ * guess_hint              - uses LP to print probabilities for the values in the specified position
+ * num_solutions           - uses exhaustive backtracking to print the number of solutions for the board
+ * autofill                - fills all empty cells with only one legal value
+ * print_board             - prints the current board
+ * undo                    - undo the last successful move on the board
+ * redo                    - redo the last successful move on the board
+ * reset                   - resets the board to its first situation
+ * mark_errors             - sets the mark_errors field to X (0 or 1)
+ * board_exit              - prints and exits the game
  * */
+
 
 
 
@@ -39,6 +44,7 @@ enum state{Init, Edit, Solve};
 enum state state;
 Node* undo_head;
 Node* redo_head;
+
 
 void insert_to_undo_lst(int command_code, int* command_data, cell cell_data);
 void insert_to_redo_lst(int command_code, int* command_data, cell cell_data);
