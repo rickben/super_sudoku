bae14f6b1b1cfb403cd9ff2b34258823d4ab5f4d
rickyBEN <ricky.benkovich@gmail.com>
2020-04-13 12:28:51 +0300

    12/4 - modified the header file of FilesAux.h

--- FilesAux.h
+++ FilesAux.h
@@ -29,22 +29,119 @@
  * trans_file_to_board                  - reads from specified file and sets the values to curr_board's board with block size of n and m
  * ***/
 
-
+/**
+ * frees all memories allocated to the curr_board
+ */
 void free_mem_board();
+
+/**
+ * saves the curr_board's board to a specified file in Solve format
+ * @param file_name string - the specified file path
+ * @return 0 if errors occurred during the save, 1 otherwise
+ */
 int solve_mode_to_file(char* file_name);
+
+/**
+ * saves the curr_board's board to a specified file in Edit format
+ * @param file_name string - the specified file path
+ * @return 0 if errors occurred during the save, 1 otherwise
+ */
 int edit_mode_to_file(char* file_name);
+
+/**
+ * saves the curr_board's board to a specified file
+ * @param file_name string - the specified file path
+ * @return 0 if errors occurred during the save, 1 otherwise
+ */
 int trans_board_to_file(char* file_name);
+
+/**
+ * allocates memory for curr_board (after scan of sizes)
+ * @param len (N) - the length of the curr_board's board
+ * @param width - the width of the curr_board's board's block
+ * @param height - the height of the curr_board's board's block
+ */
 void create_board_from_file(int len,int width, int height);
+
+/**
+ * allocates memory for temporary_board (after scan of sizes)
+ * @param len (N) - the length of the temporary_board's board
+ * @param width - the width of the temporary_board's board's block
+ * @param height - the height of the temporary_board's board's block
+ */
 void create_temporary_board_from_file(int len, int width, int height);
+
+/**
+ * copies values of cells from temporary board to curr_board
+ */
 void copy_from_temporary_to_curr_board();
+
+/**
+ * frees all memories allocated to the temporary_board
+ */
 void free_temporary_board();
+
+/**
+ * scans board's sizes from file (m n)
+ * @param in_file string - path of the specified file
+ * @return 0 if errors occurred while scanning, 1 otherwise
+ */
 int scan_size_from_file(FILE* in_file);
+
+/**
+ * scans board's values of cells in each row
+ * @param in_file - path of the specified file
+ * @param State - the state of the program to be (either Solve or Edit)
+ * @return 0 if errors occurred while scanning, 1 otherwise
+ */
 int scan_rows_from_file(FILE *in_file, enum state State);
+
+/**
+ * checks if there is a fixed cell with a value of num in the row
+ * @param row - the number of row to check
+ * @param num - the value of fixed cell to check
+ * @return 1 if there is a fixed cell with the value of num in row, 0 otherwise
+ */
 int in_row_fixed(int row, int num);
+
+/**
+ * checks if there is a fixed cell with a value of num in the column
+ * @param col - the number of column to check
+ * @param num - the value of fixed cell to check
+ * @return 1 if there is a fixed cell with the value of num in column, 0 otherwise
+ */
 int in_col_fixed(int col, int num);
+
+/**
+ * checks if there is a fixed cell with a value of num in the block
+ * @param x - the first row in block to check
+ * @param y - the first column in block to check
+ * @param num - the value of fixed cell to check
+ * @return 1 if there is a fixed cell with the value of num in block, 0 otherwise
+ */
 int in_block_fixed(int x, int y, int num);
+
+/**
+ * checks if it is valid to set the cell <j,i> with the value of num
+ * @param i - number of row
+ * @param j - number of column
+ * @param num - the value of fixed cell to check
+ * @return 1 if it is valid, 0 otherwise
+ */
 int is_valid_fixed_set(int i, int j, int num);
+
+/**
+ * check if there are erroneous fixed cell in temporary_board
+ * @return 0 if there are (it's an error), 1 otherwise
+ */
 int check_erroneous_fixed_cells_temp();
+
+/**
+ * loads the board from the specified file to curr_board's board
+ * @param file_name string - the specified file path
+ * @param State - the state of the program to be (either Edit or Solve)
+ * @return
+ */
 int trans_file_to_board(char* file_name, enum state State);
 
 #endif
