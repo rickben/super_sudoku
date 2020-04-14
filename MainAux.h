dd9c404a79aeb0cd5847c036f6976b930bec2368
matan <matanmula@mail.tau.ac.il>
2020-04-14 13:05:35 +0300

    partially moduled

--- MainAux.h
+++ MainAux.h
@@ -73,153 +73,155 @@ int check_num_of_empty_cells();
  */
 int set_random_val(int row, int col);
 
-
-void find_random_empty_cell(int* row, int* col, int num_empty);
-
-void first_set_cond_check_param(int x, int y);
-void second_set_cond_check_param(int x, int y);
-
-int fill_board_random(int x);
-void clear_cells_random(int y);
 /**
- * given a board the function in_row() checks if num exists in the given row (a row in board)
- * @return 1 if True
- * @param row
- * @param num
- * @param curr_board
+ * finds randomly an empty cell from all the empty cells
+ * and sets the value of its row and column in the pointers (parameters)
+ * @param row - the row of the cell
+ * @param col - the column of the cell
+ * @param num_empty - number of current empty cells in curr_board's board
+ */
+void find_random_empty_cell(int* row, int* col, int num_empty);
+/**
+ * checks which parameter is the first to meet the first condition
+ * @param x - the first parameter
+ * @param y - the second parameter
+ */
+void first_set_cond_check_param(int x, int y);
+/**
+ * checks which parameter is the first to meet the second condition
+ * @param x - the first parameter
+ * @param y - the second parameter
+ */
+void second_set_cond_check_param(int x, int y);
+/**
+ * fill x empty cells randomly chosen in the board with a randomly
+ * chosen valid value
+ * @param x - number of empty cells
+ * @return 1 if the set of those x cells was successful, 0 otherwise
+ */
+int fill_board_random(int x);
+
+/**
+ * choose  NxN-y cells to make empty, so that the board will have only
+ * y non-empty cells
+ * @param y - the number of non-empty cells to be
+ */
+void clear_cells_random(int y);
+
+/**
+ * check if in curr_board's board in row - there is a cell
+ * with the value of num
+ * @param row - the row to check
+ * @param num - the value of cell to check
+ * @param curr_board - the struct curr_board to check on
+ * @return 1 if exists a cell with value num in row, 0 otherwise
  */
 int in_row(int row, int num, struct curr_board* curr_board);
+
 /**
- * given a board the function in_col() checks if num exists in the given col (a column in board)
- * @return 1 if True
- * @param col
- * @param num
- * @param curr_board
+ * check if in curr_board's board in column - there is a cell
+ * with the value of num
+ * @param col - the column to check
+ * @param num - the value of cell to check
+ * @param curr_board - the struct curr_board to check on
+ * @return 1 if exists a cell with value num in column, 0 otherwise
  */
 int in_col(int col, int num, struct curr_board* curr_board);
+
 /**
- * given a board and a set of coordinates the function in_block() checks if num exists
- * in the block the coordinates belong to
- * @return 1 if True
- * @param block_x
- * @param block_y
- * @param num
- * @param curr_board
+ * check if in curr_board's board in block - there is a cell
+ * with the value of num
+ * @param block_x - the first row in block
+ * @param block_y - the first column in block
+ * @param num - the value of cell to check
+ * @param curr_board - the struct curr_board to check on
+ * @return 1 if exists a cell with value num in block, 0 otherwise
  */
 int in_block(int block_x, int block_y , int num, struct curr_board* curr_board);
+
 /**
- * given a board and a set of coordinates the function is_valid_set() checks if num exists
- * in the block the coordinates belong to or in the column, or in the row
- * @return 1 if True
- * @param x
- * @param y
- * @param num
- * @param curr_board
+ * check if it is valid to set the value of num to the cell <y,x> (<col,row>)
+ * @param x - the row of the cell
+ * @param y - the column of the cell
+ * @param num - the value to set to the cell
+ * @param curr_board - the struct curr_board to check on
+ * @return 1 if it is valid to set num to cell <y,x>, 0 otherwise
  */
 int is_valid_set(int x, int y, int num, struct curr_board* curr_board);
+
 /**
- * the function check_board_solved() scans the curr_board to check if it is solved
+ * checking if the board is solved successfully
+ * by checking if it's full and all cells valid
+ * @return 1 if it is solved, 0 otherwise
  */
 int check_board_solved();
+
 /**
- * the function separator_row() prints a separator row by the known size of curr_board
+ * prints separator row line for the board
  */
 void separator_row();
-void update_erroneous_cells();
-void cell_row(struct cell* arr);
+
 /**
- * the function board_print() prints curr_board according to the given format
+ * updates erroneous cells fields
+ * .is_erroneous to be 1, and the rest to 0
+ */
+void update_erroneous_cells();
+
+/**
+ * prints a row of cells of the curr_board's board
+ * @param arr - array that contains the row's cells
+ */
+void cell_row(struct cell* arr);
+
+/**
+ * prints the curr_board's board in the specified format
+ * by calling separator_row() and cell_row();
  */
 void board_print();
+
 /**
- * given a matrix the function fill row_pos, col_pos with the first coordinates (scanning from top left to bottom right)
- * that the matrix contains the cell value of 0
- * @return true iff a zero value cell was found
- * @param row_pos
- * @param col_pos
- * @param matrix
+ * finds the first empty cell (top-down, left-right)
+ * in the given matrix, and sets to pointers (parameters)
+ * @param row_pos - pointer to row number
+ * @param col_pos - pointer to column number
+ * @param matrix - two dimensional array of cells
+ * @return True if found an empty cell, False otherwise
  */
 bool find_empty_cell(int* row_pos, int* col_pos, cell** matrix );
+
 /**
- * the function checks if curr_board is valid (not erroneous)
- * @return true iff curr_board is valid
+ * checks if the curr_board's board is valid
+ * @return True if it is valid, 0 otherwise
  */
 bool is_valid_board();
+
 /**
- * the function checks if new_board is valid (not erroneous)
- * @return true iff new_board is valid
+ * checks if the new_board's board is valid (for num_solutions)
+ * @return True if it is valid, 0 otherwise
  */
 bool is_valid_board_new_board();
+
 /**
- * this function assigns into curr_board an empty board in size 9X9
+ * creates an empty board of size 9x9 (for empty edit command)
  */
 void create_board_size_9();
+
 /**
- * this function assigns undo_board temporally the values of curr_board
+ * fill the undo_board as curr_board before the current command
+ * will change it (only for : guess,generate);
  */
 void fill_undo_board();
+
 /**
- * this function frees undo_board
+ * free all memories allocated for undo_board
  */
 void free_undo_board();
+
 /**
- * this function fills the user linked list moves by comparing the differences between
- * undo_board and the updated curr_board
- * @param command_code
+ *
+ * fill the undo_board as curr_board after the current command
+ * changed it (only for successful : guess,generate)
+ * @param command_code - the commands code number
  */
 void fill_undo_lst_by_cmp_board(int command_code);
-/**
- * inserts new node with the command's number and its data (only successful commands)
- * @param command_code - the number of the command
- * @param command_data - its data - the cell information
- * @param cell_data - the value the cell is changed to
- */
-void insert_into_undo_lst(int command_code, int* command_data, cell cell_data);
-
-/**
- * clears from memory all the nodes between end_list (pointer to the end of the linked list)
- * and curr_move (pointer to the last move executed)
- * that way we can add to the end of the list move moves
- */
-void clear_redo_gap();
-/**
- * copies gurobi's board values (after calling solver())
- * to curr_board's board
- */
-void copy_board_to_cur();
-
-/**
- * makes all the non-empty cells in the board to be fixed
- * @param my_board - the board given
- */
-void save_all_curr_cells_fixed(cell** my_board);
-
-/**
- * checks whether the board is full - means there are no empty cells
- * @param my_board - the board given
- * @return 1 if the board is full, 0 otherwise
- */
-int check_board_full(cell** my_board);
-
-/**
- * this function copies into new_board all of curr_board data
- */
-void update_new_board_by_curr();
-/**
- * this function pops a new_board from the stack
- * pushes into the stack al the valid boards the can be created out of it
- * @return number of solved new_boards that were popped in the current call of the function
- */
-int update_stack(int count);
-
-/**
- * for every cell in curr_board, calculates all the legal values the can be set in it,
- * and assigns it in list_possible_values
- * @return 1 iff exists a cell with 1 legal value
- */
-int calculate_list_possible_values();
-/**
- * for every cell in curr_board, frees list_possible_values
- */
-void free_list_possible_values();
 #endif
